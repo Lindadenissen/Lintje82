cfda875c1c0f8b3b9a539500d5de864129e11ef5
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2021-05-17 12:26:33 +0200

    Add files via upload

---
+++ JS opdracht 1/script.js
@@ -0,0 +1,36 @@
+
+console.log('Hello Winc Academy')
+let name = 'Linda';
+console.log(name)
+/* je kunt geen if gebruiken omdat dit een keyword is*/    
+let number = 4+4;  
+console.log(number)
+let numberTwo = '4'+'4';
+console.log(numberTwo)
+/*hij maakt hier 44 van omdat JS dit als 2 indivuele getallen ziet*/
+let numberThree = 25+38;
+console.log(numberThree)
+let numberFour = 138-13;
+console.log(numberFour)
+let numberFive = 13*23;
+console.log(numberFive)
+let numberSix = 245/18;
+console.log(numberSix)
+function numberSeven (){
+var x = 5;
+var y = 10;
+x %= y;            
+console.log(numberSeven)
+}
+/*geen idee hoe ik dit moet doen*/
+
+let age = '39';
+console.log(typeof 39);
+let leeftijd = 'negenendertig';
+console.log(typeof leeftijd);
+
+
+
+
+
+
