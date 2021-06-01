64f8caa6fd7bbd12c69f9b1d026c522890b272e9
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2021-06-01 12:22:50 +0200

    Add files via upload

---
+++ opdracht/script.js
@@ -0,0 +1,13 @@
+console.log('Hallo Winc Academy studenten');
+
+//Dit is een grote som
+/*
+let multiplication = 123094 * 1823794;
+console.log ("multiplication:", multiplication);
+
+let substraction = 637263 / 54;
+console.log ("substraction:", substraction);
+*/
+
+//regel comment zie je niet omdat je hiermee de regel uitschakeld.//
+//blok comment zie je ook niet omdat je het hiermee uit uitzet. Het is alleen ter info. 
