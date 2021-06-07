34d42809bcb0b70671471dcb8c352c639b159f56
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2021-06-07 16:41:07 +0200

    Add files via upload

---
+++ JS if-else/script.js
@@ -0,0 +1,32 @@
+const age = "17";
+
+if (age >= 18){
+
+  console.log ("Deze persoon mag naar binnen");
+
+} else{
+
+  console.log ("Deze persoon mag helaas nog niet naar binnen en wordt verzocht het gebouw te verlaten");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+  console.log ("laat deze mooie vrouw binnen");
+
+    }else{
+
+  console.log ("helaas mag deze mooie vrouw niet naar binnen");
+}
+
+const driverStatus = "bob"; 
+
+if (driverStatus == "bob"){
+
+  console.log ("deze persoon is bob en mag rijden");
+
+    } else{
+
+  console.log ("deze persoon is geen bob en mag niet rijden");
+}
