cdeeb410064a5dfd67910958766a34a769cc5ca7
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2021-06-14 12:53:45 +0200

    Add files via upload

---
+++ JS function basic/script.js
@@ -0,0 +1,18 @@
+function makingPizza() {
+  console.log('stap 1: het deeg uitrollen');
+  console.log('stap 2: de tomatensaus verdelen over het deeg');
+  console.log('stap 3: de kaas verdelen over de tomatensaus');
+  console.log('stap 4: de pizza beleggen met de gewenste toevoegingen');
+}
+
+function makingSushi(){
+  console.log('stap 1: leg de rijst tussen het zeewier');
+  console.log('stap 2: rol het zeewier met de rijst op');
+  console.log('stap 3: snij de sushi in stukken');
+}
+
+makingSushi()
+makingSushi()
+makingSushi()
+makingPizza()
+makingPizza()
