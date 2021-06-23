97a7aeaedcaa64f7625b17b3830e56470c8b1c93
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2021-06-23 23:48:27 +0200

    Add files via upload

---
+++ JS calling functions excercise/script.js
@@ -0,0 +1,24 @@
+const age = function (number){
+  if (number < 18){
+    return false;
+  }else{
+    (number >= 18)
+      return true;
+    }
+  };
+
+
+const ageOldEnough = function (number){
+    if(age = true){
+      console.log ('Hello there');
+    }else {
+      console.log('Hey kiddo');
+    }
+};
+
+console.log (ageOldEnough(20));
+
+
+let includingVAT = function (baseprise, vat){
+    let result = baseprise * 1.21
+}
