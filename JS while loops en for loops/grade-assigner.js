07731060246315f0aef02ca0368a33e476b10139
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2021-06-29 13:48:42 +0200

    Add files via upload

---
+++ JS while loops en for loops/grade-assigner.js
@@ -0,0 +1,24 @@
+function assignGrade(score) {
+  if (score > 90) {
+    return "A";
+  } else if (score > 80) {
+    return "B";
+  } else if (score > 70) {
+    return "C";
+  } else if (score > 65) {
+    return "D";
+  } else {
+    return "E";
+  }
+}
+
+for (score > 90; score <= 100; score = "A"){
+  return assignGrade;
+  console.log ('The score is ' + score);
+
+    
+};
+
+
+
+ 
